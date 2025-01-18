f6c16211c59ae86b8c09586b7d10776f3af127f2
anth <phamanthony47@gmail.com>
2025-01-17 21:16:43 -0500

    React Template

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
+
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -32,4 +29,3 @@ function App() {
   )
 }
 
-export default App
