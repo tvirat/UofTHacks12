79cfc49d84fe4529b69750f32174ae40e21fcfea
CODEdragALon <102843684+CODEdragALon@users.noreply.github.com>
2025-01-18 21:16:41 -0500

    Blah

--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -1,7 +1,7 @@
 import Body from '../components/Body';
 import Container from 'react-bootstrap/Container';
 import Map from '../components/Map';
-import initMap from '../frontend/mail_pins.js';
+import initMap from '../mail_pins.js';
 
 const MapPage = () => {
     return (
