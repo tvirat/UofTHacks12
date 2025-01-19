3bb3cfedb9d10d1d35a1381ebf003729e62ab4dc
CODEdragALon <102843684+CODEdragALon@users.noreply.github.com>
2025-01-18 19:28:36 -0500

    Update mail_pins.js

--- frontend/mail_pins.js
+++ frontend/mail_pins.js
@@ -1,12 +1,30 @@
-// Create Google Map pin
-function initMap(latitude, longitude, place) {
+// Create Google Map pin with functionality (zooming in & description)
+function initMap(latitude, longitude, title) {
   const location = { lat: latitude, lng: longitude };
   const map = "https://maps.googleapis.com/maps/api/js?key=AIzaSyA2dcOrY0N80JXxMyCvkTC4LutaC21w5VE";
 
   const marker = new google.maps.marker.AdvancedMarkerElement({
-    map,
     position: location,
-    title: place,
+    map,
+    title: `${title}`,
+    content: pin.element,
+    gmpClickable:true,
+  });
+
+  map.addListener("center_changed", () => {
+    window.setTimeout(() => {
+      map.panTo(marker.position);
+    }, 3000);
+  });
+
+  marker.addListener("click", ({ domEvent, latLng }) => {
+    const { target } = domEvent;
+
+    map.setZoom(10);
+    map.setCenter(marker.position);
+
+    infoWindow.close();
+    infoWindow.setContent(marker.title);
+    infoWindow.open(marker.map, marker);
   });
 }
-window.initMap = initMap;
